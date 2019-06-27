b2b22d7fdea3cf1f11ed1fc91aa77e9c46e94a18
Rahul Chaudhary <rchaudhary@VELOCIS.IN>
2019-06-27 18:17:38 +0530

    a

--- server.js
+++ server.js
@@ -1,15 +1,41 @@
 var express=require('express');
 var app=express();
+var bodyParser = require('body-parser');
+var multer  = require('multer');
+var fs = require("fs");
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+var upload = multer({ dest: './uploads' });
 app.use(express.static('public'));
+
 app.get("/index.htm",function(req,res){
 		res.sendFile(__dirname+"/"+"index.htm");
-})
+});
 
-app.get("/process_get",function(req,res){
-	responce={
-			 first_name:req.query.first_name,
-			 last_name:req.query.last_name
+app.get("/formfile.htm",function(req,res){
+		res.sendFile(__dirname+"/"+"formfile.htm");
+		//res.render('formfile.htm');
+});
+
+app.post("/process_post",urlencodedParser,function(req,res){
+	response={
+			 first_name:req.body.first_name,
+			 last_name:req.body.last_name
 			 };
 
-		console.log(responce);
-})
+		res.end(JSON.stringify(response));
+});
+
+app.post("/file_upload",function(req,res){
+	console.log(req.file.name);
+    console.log(req.file.path);
+    console.log(req.file.type);
+});
+
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+   
+   console.log("Example app listening at http://%s:%s", host, port)
+});
