b11bb54fd95d9705e3433783339a90d819da456d
Rahul Chaudhary <rchaudhary@VELOCIS.IN>
2019-06-28 16:18:26 +0530

    a

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ var app=express();
 var bodyParser = require('body-parser');
 var multer  = require('multer');
 var fs = require("fs");
+var cookieParser = require('cookie-parser');
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 var upload = multer({ dest: './uploads' });
+app.use(cookieParser());
 app.use(express.static('public'));
 
 app.get("/index.htm",function(req,res){
@@ -28,6 +30,7 @@ app.post("/process_post",urlencodedParser,function(req,res){
 });
 
 app.post("/file_upload",function(req,res){
+  console.log("Cookies: ", req.cookies)
 	var storage = multer.diskStorage({
                 destination: function (req, file, callback) {
                 callback(null, './uploads');
@@ -40,19 +43,71 @@ app.post("/file_upload",function(req,res){
   var upload = multer({ storage: storage }).single('file');
   upload(req, res, function (err) 
     {
-        console.log(req.file);
+        //console.log(req.file);
         console.log(req.body);
         if (err) 
         {
             console.log("Photo API ERROR: "+err);
             return res.end("Error uploading file.");
         }
-        console.log("SUCCESS");
+       // console.log(res);
+        // res.end(JSON.stringify(res));
        
        
        // res.json("File is uploaded");
     });
 });
+
+app.get('/userlists',function(req,res){
+  fs.readFile(__dirname + "/" + "user.json", 'utf8', function (err, data){
+      console.log(data);
+      res.end(data);
+   });
+})
+
+// add user list 
+
+var user = {
+   "user4" : {
+      "name" : "mohit",
+      "password" : "password4",
+      "profession" : "teacher",
+      "id": 4
+   }
+}
+
+app.post('/addUser',function(req,res){
+  // First read existing users.
+  fs.readFile( __dirname + "/" + "user.json", 'utf8', function (err, data) {
+      data = JSON.parse(data);
+      data["user4"] = user["user4"];
+      console.log(data);
+      res.end(JSON.stringify(data));
+   });
+
+})
+
+app.get('/:id', function (req, res) {
+   // First read existing users.
+   fs.readFile( __dirname + "/" + "user.json", 'utf8', function (err, data) {
+      var users = JSON.parse( data );
+      var user = users["user" + req.params.id] 
+      console.log( user );
+      res.end( JSON.stringify(user));
+   });
+})
+
+var id=2;
+app.delete('/deleteUser', function (req, res) {
+   // First read existing users.
+   fs.readFile( __dirname + "/" + "user.json", 'utf8', function (err, data) {
+      data = JSON.parse(data);
+      delete data["user" + 2];
+       
+      console.log(data);
+      res.end( JSON.stringify(data));
+   });
+})
 
 var server = app.listen(8081, function () {
    var host = server.address().address
